33608bae8a1f8df9847aba170e374689a87ebd28
Ricardo Pereira <patresio@gmail.com>
2024-11-26 14:24:58 -0300

    fix: bug error function type

--- packages/core/src/event/index.ts
+++ packages/core/src/event/index.ts
@@ -1,7 +1,7 @@
-import type createEmptyEvent from './functions/create-empty-event'
-import type createEmptyGuest from './functions/create-empty-guest'
-import type validateEventDataConsistency from './functions/validate-event-data-consistency'
-import type validateGuestDataConsistency from './functions/validate-guest-data-consistency'
+import createEmptyEvent from './functions/create-empty-event'
+import createEmptyGuest from './functions/create-empty-guest'
+import validateEventDataConsistency from './functions/validate-event-data-consistency'
+import validateGuestDataConsistency from './functions/validate-guest-data-consistency'
 import Event from './model/event'
 import Guest from './model/guest'
 
