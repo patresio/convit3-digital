fe46dccfc0709cbac36f1f605be7119cfe52929e
Ricardo Pereira <patresio@gmail.com>
2024-12-03 09:39:40 -0300

    feat: without events in the events

---
+++ apps/mobile/components/event/without-event.tsx
@@ -0,0 +1,40 @@
+import { Image, Text, View } from 'react-native'
+import {
+  bgZinc950,
+  fontBold,
+  gapY4,
+  itemsCenter,
+  py4,
+  roundedLg,
+  text3Xl,
+  textLg,
+  textWhite,
+  textZinc400,
+  wFull
+} from '@/style'
+
+const WithoutEvent = () => {
+  return (
+    <View style={[itemsCenter]}>
+      <View style={[itemsCenter, py4, bgZinc950, wFull, roundedLg, gapY4]}>
+        <Image
+          source={require('@/assets/images/sem-eventos.png')}
+          style={{ width: 200, height: 200 }}
+        />
+        <Text style={[textWhite, text3Xl, fontBold]}>OOPS....</Text>
+        <View>
+          <Text style={[textZinc400, textLg]}>
+            Ainda não tem nenhum evento cadastrado
+          </Text>
+          <Text style={[textZinc400, textLg]}>Vamos escanear um QR Code?</Text>
+        </View>
+        <Image
+          source={require('@/assets/images/triangulo.png')}
+          style={{ width: 25, height: 17 }}
+        />
+      </View>
+    </View>
+  )
+}
+
+export default WithoutEvent
